5f0848474316c828d59cfdb754b8ddb3caa5689c
Igor Guliaev <igor.s.gulyaev@gmail.com>
2026-01-12 19:09:13 +0000

    Add show page for transaction

--- apps/web-next/src/pages/transactions/show.tsx
+++ apps/web-next/src/pages/transactions/show.tsx
@@ -1,5 +1,58 @@
-import { AntdInferencer } from "@refinedev/inferencer/antd";
+import React from "react";
+import { useShow, useOne } from "@refinedev/core";
+import {
+  Show,
+  TagField,
+  TextField,
+  DateField,
+  NumberField,
+} from "@refinedev/antd";
+import { Typography } from "antd";
+
+const { Title } = Typography;
 
 export const TransactionShow = () => {
-    return <AntdInferencer />;
+  const {
+    result: record,
+    query: { isLoading },
+  } = useShow();
+
+  const {
+    result: categoryData,
+    query: { isLoading: categoryIsLoading },
+  } = useOne({
+    resource: "categories",
+    id: record?.category_id || "",
+    queryOptions: {
+      enabled: !!record,
+    },
+  });
+
+  const {
+    result: bankAccountData,
+    query: { isLoading: bankAccountIsLoading },
+  } = useOne({
+    resource: "bank_accounts",
+    id: record?.bank_account_id || "",
+    queryOptions: {
+      enabled: !!record,
+    },
+  });
+
+  return (
+    <Show isLoading={isLoading}>
+      <Title level={5}>Date</Title>
+      <DateField value={record?.date} />
+      <Title level={5}>Type</Title>
+      <TextField value={record?.type} />
+      <Title level={5}>Category</Title>
+      {categoryIsLoading ? <>Loading...</> : <>{categoryData?.name}</>}
+      <Title level={5}>Amount</Title>
+      <NumberField value={record?.amount ?? ""} />
+      <Title level={5}>Bank Account</Title>
+      {bankAccountIsLoading ? <>Loading...</> : <>{bankAccountData?.name}</>}
+      <Title level={5}>Notes</Title>
+      <TextField value={record?.notes} />
+    </Show>
+  );
 };
