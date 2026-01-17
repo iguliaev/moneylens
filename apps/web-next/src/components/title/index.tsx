b654edb630b2f2802a20b5d9cc8779c5b9621dd3
Igor Guliaev <igor.s.gulyaev@gmail.com>
2026-01-17 19:56:04 +0000

    Add title component

---
+++ apps/web-next/src/components/title/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { theme } from "antd";
+
+export const ProjectTitle: React.FC = () => {
+  const { token } = theme.useToken();
+  const textColor = token.colorTextHeading;
+
+  return (
+    <svg
+      width="140"
+      height="32"
+      viewBox="0 0 140 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <text
+        x="0"
+        y="24"
+        fontFamily="Arial, Helvetica, sans-serif"
+        fontSize="24"
+        fontWeight="bold"
+        fill={textColor}
+      >
+        MoneyLens
+      </text>
+    </svg>
+  );
+};
