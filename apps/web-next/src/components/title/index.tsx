fb1b11c2fdabcf4fc35fc5e38cf0869d21624fff
Igor Guliaev <igor.s.gulyaev@gmail.com>
2026-01-18 18:08:45 +0000

    Add a link to the main page to logo

--- apps/web-next/src/components/title/index.tsx
+++ apps/web-next/src/components/title/index.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { theme } from "antd";
-
+import { Link } from "@refinedev/core";
 export const ProjectTitle: React.FC = () => {
   const { token } = theme.useToken();
   const textColor = token.colorTextHeading;
 
   return (
-    <svg
-      width="140"
-      height="32"
-      viewBox="0 0 140 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <text
-        x="0"
-        y="24"
-        fontFamily="Arial, Helvetica, sans-serif"
-        fontSize="24"
-        fontWeight="bold"
-        fill={textColor}
+    <Link to="/">
+      <svg
+        width="140"
+        height="32"
+        viewBox="0 0 140 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        MoneyLens
-      </text>
-    </svg>
+        <text
+          x="0"
+          y="24"
+          fontFamily="Arial, Helvetica, sans-serif"
+          fontSize="24"
+          fontWeight="bold"
+          fill={textColor}
+        >
+          MoneyLens
+        </text>
+      </svg>
+    </Link>
   );
 };
