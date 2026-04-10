597a2427f5f48d18463dca301d66184ec3b40dd3
Igor Guliaev <igor.s.gulyaev@gmail.com>
2026-04-10 21:01:22 +0100

    Revert "feat(charts): use linear interpolation for sharper trendlines"
    This reverts commit 582f3346e66f166aa7cb787824f14e32017a0816.

--- apps/web-next/src/pages/dashboard/ChartsTab.tsx
+++ apps/web-next/src/pages/dashboard/ChartsTab.tsx
@@ -313,7 +313,7 @@ const SpendingTrendlineChart = ({
             {selected.map((item, i) => (
               <Line
                 key={item}
-                type="linear"
+                type="monotone"
                 dataKey={item}
                 stroke={DONUT_COLORS[i % DONUT_COLORS.length]}
                 strokeWidth={2.5}
